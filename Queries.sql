51ce5f7cbd3020c548be89aadee8593c903fae0e
Jesse Boogaard <jesse98@live.nl>
2019-01-03 20:56:41 +0100

    added file for queries and fixed database model

---
+++ Queries.sql
@@ -0,0 +1,3 @@
+﻿SELECT ProductCollection.Product_ID, ProductCollection.Collection_ID, Product.ProductName, Product.ProductNo FROM ((ProductCollection
+INNER JOIN UserCollection ON ProductCollection.Collection_ID = UserCollection.Id)
+INNER JOIN Product ON ProductCollection.Product_ID = Product.Id)
