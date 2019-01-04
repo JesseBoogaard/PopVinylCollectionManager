cd2f1a95c01205223ad008705e1e9cd2b7be4aa8
Jesse Boogaard <jesse98@live.nl>
2019-01-04 13:09:30 +0100

    added several functions regarding collection registration

--- PopVinylCollectionManager/Form1.cs
+++ PopVinylCollectionManager/Form1.cs
@@ -10,8 +10,27 @@ using System.Windows.Forms;
 
 namespace PopVinylCollectionManager {
     public partial class Form1 : Form {
+        public User Usr = new User("Jesse", 1);
         public Form1() {
             InitializeComponent();
         }
+
+
+        private void CreateCollButton_Click(object sender, EventArgs e) {
+            OpenCreateCollForm();
+        }
+
+        private void OpenCreateCollForm() {
+            Form2 f = new Form2(Usr, this);
+            f.Show();
+        }
+
+        public void UpdateCollectionListBox() {
+            CollectionListBox.Items.Clear();
+            foreach(Collection c in Usr.GetAllUserCollections) {
+                CollectionListBox.Items.Add(c);
+            }
+        }
+
     }
 }
