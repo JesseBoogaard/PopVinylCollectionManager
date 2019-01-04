055d1d0fa55674278b56ca362f4330623304af42
Jesse Boogaard <jesse98@live.nl>
2019-01-04 12:13:05 +0100

    added proper query for getting usercollection by ID

--- Queries.sql
+++ Queries.sql
@@ -1,3 +1,9 @@
 ﻿SELECT ProductCollection.Product_ID, ProductCollection.Collection_ID, Product.ProductName, Product.ProductNo FROM ((ProductCollection
 INNER JOIN UserCollection ON ProductCollection.Collection_ID = UserCollection.Id)
 INNER JOIN Product ON ProductCollection.Product_ID = Product.Id)
+
+/* get collection from user where ID is a match */
+SELECT Usr.Name, UserCollection.CollectionName, UserCollection.CollectionInfo, Product.ProductName, Product.ProductNo FROM (((ProductCollection
+INNER JOIN UserCollection ON ProductCollection.Collection_ID = UserCollection.Id)
+INNER JOIN Product ON ProductCollection.Product_ID = Product.Id)
+INNER JOIN Usr ON UserCollection.User_ID = Usr.Id)
