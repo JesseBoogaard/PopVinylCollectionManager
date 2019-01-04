39561d4680f78812239f2489f713af37b34d0b55
Jesse Boogaard <jesse98@live.nl>
2019-01-04 17:51:49 +0100

    more example queries

--- Queries.sql
+++ Queries.sql
@@ -9,8 +9,11 @@ INNER JOIN Product ON ProductCollection.Product_ID = Product.Id)
 INNER JOIN Usr ON UserCollection.User_ID = Usr.Id)
 
 /* get collection by collectionname and order results by their name in descending order */
-SELECT Product.ProductName, Product.ProductNo FROM ((ProductCollection
+SELECT Product.ProductName AS 'Product Name', Product.ProductNo AS 'Product Number' FROM ((ProductCollection
 INNER JOIN UserCollection ON ProductCollection.Collection_ID = UserCollection.Id)
 INNER JOIN Product ON ProductCollection.Product_ID = Product.Id) WHERE ProductCollection.Collection_ID = (SELECT Id FROM UserCollection WHERE CollectionName = 'TestCollection')
 GROUP BY Product.ProductName, Product.ProductNo
 ORDER BY ProductName DESC
+
+/* get count of unique figures in user collection */
+SELECT DISTINCT COUNT(ProductCollection.Product_ID) AS 'Unique figures' FROM ProductCollection WHERE Collection_ID = (SELECT Id FROM UserCollection WHERE CollectionName = 'TestCollection')
