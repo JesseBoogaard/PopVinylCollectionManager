483be7b8c9bc72666355c15927d9dd00580a2dae
Jesse Boogaard <jesse98@live.nl>
2019-01-05 14:54:21 +0100

    added basics for series class

---
+++ PopVinylCollectionManager/Series.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PopVinylCollectionManager {
+    class Series {
+
+        private int _ID;
+        private string _Name;
+
+        public Series(int ID, string Name) {
+            _ID = ID;
+            _Name = Name;
+        }
+    }
+}
