8fb9e8b76d6dd588ff5ae19a20bc76a80c508e14
Jesse Boogaard <jesse98@live.nl>
2019-01-05 19:47:29 +0100

    added getter for products to collection class

--- PopVinylCollectionManager/Collection.cs
+++ PopVinylCollectionManager/Collection.cs
@@ -9,7 +9,7 @@ namespace PopVinylCollectionManager {
         private string _Name { get; set; }
         private string _Info { get; set; }
         private int Id { get; }
-        private List<Product> _Products { get; set; } = new List<Product>();
+        private List<Product> _Products = new List<Product>();
 
         public Collection(string Name, string Info) {
             _Name = Name;
@@ -19,6 +19,12 @@ namespace PopVinylCollectionManager {
         public string Name {
             get {
                 return _Name;
+            }
+        }
+
+        public List<Product> Products {
+            get {
+                return _Products;
             }
         }
 
