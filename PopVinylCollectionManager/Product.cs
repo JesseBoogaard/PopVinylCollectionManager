d24959c3523351f61bfca325ad9da235e0b104c2
Jesse Boogaard <jesse98@live.nl>
2019-01-05 19:30:03 +0100

    added to string method for Product class

--- PopVinylCollectionManager/Product.cs
+++ PopVinylCollectionManager/Product.cs
@@ -13,8 +13,12 @@ namespace PopVinylCollectionManager {
 
         public Product(string Name, int ProductNo, string Series) {
             _Name = Name;
-            _Series = Series;
             _ProductNo = ProductNo;
+            _Series = Series;
+        }
+
+        public override string ToString() {
+            return "Name: " + _Name + "\t ProductNo.: " + _ProductNo + "\t Series: " + _Series;
         }
     }
 }
