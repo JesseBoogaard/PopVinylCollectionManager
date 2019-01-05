114b5edcccfeb880b83539fcb70769d321a3c9d8
Jesse Boogaard <jesse98@live.nl>
2019-01-05 19:46:50 +0100

    added new form for adding product to user collection

---
+++ PopVinylCollectionManager/AddToCollectionForm.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PopVinylCollectionManager {
+    public partial class AddToCollectionForm : Form {
+        private MainForm _M;
+        public AddToCollectionForm(MainForm m) {
+            _M = m;
+            InitializeComponent();
+        }
+    }
+}
