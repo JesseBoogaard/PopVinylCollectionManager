4be41fb1eb8f2a003df4c776a477ab48507ef34b
Jesse Boogaard <jesse98@live.nl>
2019-01-06 18:16:31 +0100

    added edit collection form

---
+++ PopVinylCollectionManager/EditCollectionForm.Designer.cs
@@ -0,0 +1,41 @@
+﻿namespace PopVinylCollectionManager {
+    partial class EditCollectionForm {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            this.SuspendLayout();
+            // 
+            // EditCollectionForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Name = "EditCollectionForm";
+            this.Text = "Edit Collection";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
